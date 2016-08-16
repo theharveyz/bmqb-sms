2351201549639f37aa095c77fee5aee15de99506
张华伟 <harveyz@zhanghuaweideMacBook-Pro.local>
2016-08-16 18:16:27 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 var fs = require('fs');
 var path = require('path');
+
 try{
   fs.accessSync(path.join('./lib/', 'index.js'), fs.constants.R_OK);
-  
-  // 兼容新的接口
-  if (!global._babelPolyfill) {
-    require('babel-polyfill');
-  }
 
   exports = module.exports = require('./lib');
 } catch (e) {
   exports = module.exports = require('./src');
+}
+
+try {
+  // 兼容新的接口
+  if (!global._babelPolyfill) {
+    require('babel-polyfill');
+  }
+} catch (e) {
+  // ...
 }
